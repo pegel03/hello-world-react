98be029cd5b0b08039d806f0bfc15a575541563f
Pegel03 <pegel03@gmail.com>
2018-02-18 11:01:40 +0100

    Added Block matrix and styling

--- client/components/App/App.jsx
+++ client/components/App/App.jsx
@@ -5,24 +5,23 @@ import React, { PureComponent } from 'react';
 import classNames from 'classnames';
 
 import classes from './App.less';
+import Block from "../Block";
 
 export default class App extends PureComponent {
 
-
     render() {
-        // var me = ; // => 'foo bar'
-        var deze = classNames('hoofd');
-// console.log('pgl ', deze);
-        var ik = 'lukt wel ';
+        var classnames = classNames('container');
+
+        const bloks = ['nr1', 'nr2', 'nr3', 'nr4', 'nr5', 'nr6', 'nr7', 'nr8', 'nr9', 'nr10', 'nr11', 'nr12'];
 
         return (
-            <div className={deze} >
-                <div className="links" >
+            <div className={classnames} >
+                <div className="left" >
                     <h1>Links dus</h1>
                     <h1>Hello to you too:) nr two</h1>
                 </div>
-                <div className="rechts" >
-                    <h1>Rechts</h1>
+                <div className="right" >
+                    {bloks.map((b, index) => <Block key={index} naam={b} />)}
                 </div>
             </div>
         );
